083413cfdf563bd401676d657f667aab6e0ab97d
gzitcast <itcast@itcast.cn>
2015-03-27 15:59:38 +0800

    WeChat_头像上传

--- WeChat/Classes/WCProfileViewController.m
+++ WeChat/Classes/WCProfileViewController.m
@@ -10,7 +10,7 @@
 #import "XMPPvCardTemp.h"
 #import "WCEditVCardViewController.h"
 
-@interface WCProfileViewController()<WCEditVCardViewControllerDelegate>
+@interface WCProfileViewController()<WCEditVCardViewControllerDelegate,UIActionSheetDelegate,UINavigationControllerDelegate,UIImagePickerControllerDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *avatarImgView;//头像
 @property (weak, nonatomic) IBOutlet UILabel *nicknameLabel;//昵称
 @property (weak, nonatomic) IBOutlet UILabel *wechatNumLabel;//微信号
@@ -85,6 +85,7 @@
     switch (selectedCell.tag) {
         case 0:
             WCLog(@"换头像");
+            [self choseImg];
             break;
         case 1:
             WCLog(@"进入下一个控制器");
@@ -97,6 +98,54 @@
             break;
     }
 
+}
+
+#pragma mark 选择图片
+-(void)choseImg{
+    UIActionSheet *sheet = [[UIActionSheet alloc] initWithTitle:@"请选择" delegate:self cancelButtonTitle:@"取消" destructiveButtonTitle:@"照相" otherButtonTitles:@"图片库", nil];
+    [sheet showInView:self.view];
+}
+
+
+-(void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex{
+    WCLog(@"%ld",buttonIndex);
+    if (buttonIndex == 2) return;
+    
+    // 图片选择器
+    UIImagePickerController *imgPC = [[UIImagePickerController alloc] init];
+    
+    //设置代理
+    imgPC.delegate = self;
+   
+    //允许编辑图片
+    imgPC.allowsEditing = YES;
+    
+    if (buttonIndex == 0) {//照相
+        imgPC.sourceType =  UIImagePickerControllerSourceTypeCamera;
+    }else{//图片库
+        imgPC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+    
+    //显示控制器
+    [self presentViewController:imgPC animated:YES completion:nil];
+}
+
+
+#pragma mark 图片选择器的代理
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
+    WCLog(@"%@",info);
+    
+    //获取修改后的图片
+    UIImage *editedImg = info[UIImagePickerControllerEditedImage];
+    
+    //更改cell里的图片
+    self.avatarImgView.image = editedImg;
+    
+    //移除图片选择的控制器
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+    //把新的图片保存到服务器
+    [self editVCardViewController:nil didFinishedSave:nil];
 }
 
 
@@ -122,6 +171,9 @@
 
     //获取当前电子名片
     XMPPvCardTemp *myVCard = [WCXMPPTool sharedWCXMPPTool].vCard.myvCardTemp;
+    
+    //重新设置头像
+    myVCard.photo = UIImageJPEGRepresentation(self.avatarImgView.image, 0.75);
     
     //重新设置myVCard里的属性
     myVCard.nickname = self.nicknameLabel.text;
