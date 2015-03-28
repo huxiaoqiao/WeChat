82f040d25b595e755d98d7826585a5e2d89a6243
gzitcast <itcast@itcast.cn>
2015-03-28 12:03:34 +0800

    WeChat_发送聊天数据

--- WeChat/Classes/WCChatViewController.m
+++ WeChat/Classes/WCChatViewController.m
@@ -8,11 +8,16 @@
 
 #import "WCChatViewController.h"
 
-@interface WCChatViewController()<NSFetchedResultsControllerDelegate>
+@interface WCChatViewController()<NSFetchedResultsControllerDelegate,UITableViewDataSource,UITableViewDelegate,UITextFieldDelegate>
 
 {
     NSFetchedResultsController *_resultContr;
 }
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+/**
+ * 输入框容器距离底部的约束
+ */
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottomConstraint;
 
 @end
 
@@ -22,6 +27,11 @@
 -(void)viewDidLoad{
 
     [super viewDidLoad];
+    
+    //添加键盘通知
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(kbWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(kbWillHide:) name:UIKeyboardWillHideNotification object:nil];
+    
     
     //加载数据库的聊天数据
     
@@ -50,8 +60,80 @@
     WCLog(@"%@",_resultContr.fetchedObjects);
 }
 
-
+#pragma mark 数据库内容改变调用
 -(void)controllerDidChangeContent:(NSFetchedResultsController *)controller{
     
+    [self.tableView reloadData];
+    
+    //表格滚动到底部
+    NSIndexPath *lastIndex = [NSIndexPath indexPathForRow:_resultContr.fetchedObjects.count - 1 inSection:0];
+    [self.tableView scrollToRowAtIndexPath:lastIndex atScrollPosition:UITableViewScrollPositionBottom animated:YES];
+    
+}
+#pragma mark -tableView DataSource
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return _resultContr.fetchedObjects.count;
+}
+
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    static NSString *ID = @"ChatCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID
+                             ];
+    
+    //获取聊天信息
+    XMPPMessageArchiving_Message_CoreDataObject *msgObj = _resultContr.fetchedObjects[indexPath.row];
+    
+    
+    cell.textLabel.text = msgObj.body;
+    
+    
+    return cell;
+}
+
+#pragma mark -键盘的通知
+#pragma mark 键盘将显示
+-(void)kbWillShow:(NSNotification *)noti{
+    //显示的时候改变bottomContraint
+    
+    // 获取键盘高度
+    NSLog(@"%@",noti.userInfo);
+    CGFloat kbHeight = [noti.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue].size.height;
+    
+    
+    self.bottomConstraint.constant = kbHeight;
+}
+
+
+#pragma mark 键盘将隐藏
+-(void)kbWillHide:(NSNotification *)noti{
+    self.bottomConstraint.constant = 0;
+    
+}
+
+
+#pragma mark 发送聊天数据
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    
+    NSString *txt = textField.text;
+    
+    //怎么发聊天数据
+    XMPPMessage *msg = [XMPPMessage messageWithType:@"chat" to:self.friendJid];
+    [msg addBody:txt];
+    [[WCXMPPTool sharedWCXMPPTool].xmppStream sendElement:msg];
+    
+    // 清空输入框的文本
+    textField.text = nil;
+    
+    return YES;
+
+}
+
+
+#pragma mark 表格滚动，隐藏键盘
+-(void)scrollViewWillBeginDragging:(UIScrollView *)scrollView{
+    [self.view endEditing:YES];
+
 }
 @end
